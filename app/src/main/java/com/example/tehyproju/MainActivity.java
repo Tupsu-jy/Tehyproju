c42db31b839afe7ea7370bf11c91ee11c276c0d5
ReijaP <58248828+ReijaP@users.noreply.github.com>
2019-12-10 09:24:07 +0200

    reset

--- app/src/main/java/com/example/tehyproju/MainActivity.java
+++ app/src/main/java/com/example/tehyproju/MainActivity.java
@@ -15,6 +15,7 @@ import com.example.tehyproju.db.QuestRDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    Counters page0 = Counters.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
     /** Ohjelma avaa Lomake1-luokan, kun käyttäjä painaa nappia
      * Napin painallus aloittaa uuden Intent:in
+     * Kyselylomakkeen avaus aloittaa pisteidenlaskun aina alusta
      * @param view button-elementti
      */
     public void lisaaMerkinta(View view){
         Intent intent1 = new Intent(getApplicationContext(), Lomake1.class);
         startActivity(intent1);
+        page0.reset();
     }
 
     /** Ohjelma avaa vinkit-luokan, kun käyttäjä painaa nappia
